b5af07125e4331dbe1657982b94615e22bf4c267
n-lisichko <n.lisichko@protonmail.com>
2020-05-01 21:14:28 +0300

    v 11

--- dkey.js
+++ dkey.js
@@ -1,34 +1,29 @@
-// #5c503d - dark outlines and borders (bookmarks)
-
 function posressel() {
-	var elements = document.getElementsByClassName("restaxon"); // get all result taxa and reset colour
+	var elements = document.getElementsByClassName("restaxon");
 	for(var i = 0; i < elements.length; i++){
-		elements[i].style.backgroundColor = "#f5f3f0"; // initial colour of results
+		elements[i].style.backgroundColor = "#f5f3f0"; 
 		elements[i].style.borderLeft = "7px solid #f5f3f0";
-		elements[i].style.color = "grey";
 	}
 	for (var k = 0, j = arguments.length; k < j; k++){
-	var elements = document.getElementsByClassName(arguments[k]); // get all elements from args, colorize
+	var elements = document.getElementsByClassName(arguments[k]); 
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "#ecc893";
 		elements[i].style.borderLeft = "7px solid #98661b";
-		elements[i].style.color = "black";
 	}
 	}
-	// 
 }
 
 function posstesel() {
-	var elements = document.getElementsByClassName("thesis"); // get all result taxa and reset colour
+	var elements = document.getElementsByClassName("thesis");
 	for(var i = 0; i < elements.length; i++){
-		elements[i].style.backgroundColor = "#f5f3f0"; // initial colour of results
+		elements[i].style.backgroundColor = "#f5f3f0";
 	}
-	var elements = document.getElementsByClassName("antithesis"); // get all result taxa and reset colour
+	var elements = document.getElementsByClassName("antithesis");
 	for(var i = 0; i < elements.length; i++){
-		elements[i].style.backgroundColor = "#f5f3f0"; // initial colour of results
+		elements[i].style.backgroundColor = "#f5f3f0";
 	}
 	for (var k = 0, j = arguments.length; k < j; k++){
-	var elements = document.getElementsByClassName(arguments[k]); // get all elements from args, colorize
+	var elements = document.getElementsByClassName(arguments[k]);
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "#ecc893";
 	}
@@ -36,7 +31,6 @@ function posstesel() {
 	
 }
 
-// bookmark set
 function stbmset() {
 		for (var k = 0, j = arguments.length; k < j; k++){
 	var elements = document.getElementsByClassName(arguments[k]); 
@@ -119,10 +113,12 @@ function resetsteps() {
 	var elements = document.getElementsByClassName("thesis");
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "#f5f3f0"; 
+		elements[i].style.outline = "none";
 	}
 	var elements = document.getElementsByClassName("antithesis");
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.backgroundColor = "#f5f3f0";
+		elements[i].style.outline = "none";
 	}
 	var elements = document.getElementsByClassName("couplet");
 	for(var i = 0; i < elements.length; i++){
@@ -136,15 +132,7 @@ function resetsteps() {
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.display = "inline";
 	}
-	var elements = document.getElementsByClassName("thesis");
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.outline = "none";
-	}
 	
-	var elements = document.getElementsByClassName("antithesis");
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.outline = "none";
-	}
 	var elements = document.getElementsByClassName("lead");
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.outline = "none";
@@ -204,5 +192,126 @@ function setlngeng() {
 	var elements = document.getElementsByClassName("itxtua"); 
 	for(var i = 0; i < elements.length; i++){
 		elements[i].style.display = "none";
+	}
+}
+
+function addregsel(){
+	for (var k = 0, j = arguments.length; k < j; k++){
+	var elements = document.getElementsByClassName(arguments[k]); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "100%";
+		elements[i].style.color = "#000000";
+	}
+	}
+}
+
+function startregsel() {
+	var elements = document.getElementsByClassName("regionshide"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "inline";
+	}
+	var elements = document.getElementsByClassName("regionselector"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "block";
+	}
+	var elements = document.getElementsByClassName("regionsactivate"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "none";
+	}
+	var elements = document.getElementsByClassName("thesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+	var elements = document.getElementsByClassName("antithesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+	var elements = document.getElementsByClassName("restaxon"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+}
+
+function stopregsel() {
+	var elements = document.getElementsByClassName("regionshide"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "none";
+	}
+	var elements = document.getElementsByClassName("regionselector"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "none";
+	}
+	var elements = document.getElementsByClassName("regionsactivate"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "inline";
+	}
+	var elements = document.getElementsByClassName("thesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "100%";
+		elements[i].style.color = "#000000";
+	}
+	var elements = document.getElementsByClassName("antithesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "100%";
+		elements[i].style.color = "#000000";
+	}
+	var elements = document.getElementsByClassName("restaxon"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "100%";
+		elements[i].style.color = "#000000";
+	}
+}
+
+function markregsel(){
+	for (var k = 0, j = arguments.length; k < j; k++){
+	var elements = document.getElementsByClassName(arguments[k]); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#ffd11a";
+	}
+	}
+}
+
+function regreset() {
+	var elements = document.getElementsByClassName("textbuttonb"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "#e0dad1";
+	}
+	var elements = document.getElementsByClassName("thesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+	var elements = document.getElementsByClassName("antithesis"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+	var elements = document.getElementsByClassName("restaxon"); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.opacity = "40%";
+		elements[i].style.color = "#c2b6a3";
+	}
+}
+
+function closeimg(){
+	var elements = document.getElementsByClassName("figdiv");
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "none";
+	}
+}
+
+function showfig(){
+	var elements = document.getElementsByClassName("figdiv");
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "none";
+	}
+	for (var k = 0, j = arguments.length; k < j; k++){
+	var elements = document.getElementsByClassName(arguments[k]); 
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.display = "block";
+	}
 	}
 }
